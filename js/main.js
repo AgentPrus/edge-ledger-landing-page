b0fd5fdb4d695c50d0938135e1c0560cee275b61
AgentPrus <nikitaprus98@gmail.com>
2020-04-14 08:42:56 +0300

    add smooth scroll and transperent nav on scroll

--- js/main.js
+++ js/main.js
@@ -12,3 +12,32 @@ function initMap() {
     position: loc, map: map
   })
 }
+
+// Sticky menu background
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 150) {
+    this.document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    this.document.querySelector("#navbar").style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+$("#navbar a, .btn").on('click', function (event) {
+
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 80
+      }, 800);
+  } // End if
+});
